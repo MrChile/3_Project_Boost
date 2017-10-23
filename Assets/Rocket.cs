d527573d5582c7e0249f7983bac6a67668b0754e
DESKTOP-KMVE91F\Matthew <matthew@temuco.com>
2017-10-23 14:56:50 +1100

    L41- Added Audio to Thruster

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
     Rigidbody rigidbody;
+    AudioSource audioSource;
 
 	// Use this for initialization
 	void Start () {
         rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -23,7 +25,16 @@ public class Rocket : MonoBehaviour {
         if(Input.GetKey(KeyCode.Space))
         {
             rigidbody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
         }
+        else
+        {
+            audioSource.Stop();
+        }
+
         if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
         {
             transform.Rotate(Vector3.forward);
